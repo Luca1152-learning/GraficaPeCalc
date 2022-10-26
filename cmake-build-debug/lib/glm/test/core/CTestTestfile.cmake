835caa03e22d7e58cbe9c63f8549dc6acfe36ca9
Luca Trușcă <t.luca1152@gmail.com>
2022-10-26 19:32:49 +0300

    Ignore unwanted files

--- cmake-build-debug/lib/glm/test/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tluca/Desktop/GC/lib/glm/test/core
-# Build directory: C:/Users/tluca/Desktop/GC/cmake-build-debug/lib/glm/test/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
