071c61c29396b8e41f106c300b24beeb3c8257cf
Luca Trușcă <t.luca1152@gmail.com>
2022-10-26 08:33:23 +0300

    RGB cube solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,122 @@
-// Codul sursa este adaptat dupa OpenGLBook.com
-#include <stdlib.h> // necesare pentru citirea shader-elor
+#include <windows.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <GL/glew.h> // glew apare inainte de freeglut
-#include <GL/freeglut.h> // nu trebuie uitat freeglut.h
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
-//////////////////////////////////////
+// New OpenGL
+GLuint VboId1,
+        ColorBufferId1,
+        VertexShaderId,
+        FragmentShaderId,
+        ProgramId;
 
-GLuint VaoId, VboId, ColorBufferId, VertexShaderId, FragmentShaderId, ProgramId;
-
-///////////////////////////////////////////
-
-// Shader-ul de varfuri / Vertex shader (este privit ca un sir de caractere)
 const GLchar *VertexShader = {
-        "#version 400\n"
+        "#version 400\n"\
+        "layout(location=0) in vec4 in_Position;\n"\
+        "layout(location=1) in vec4 in_Color;\n"\
+        "out vec4 ex_Color;\n"\
+        "void main(void)\n"\
+        "{\n"\
+        "  gl_Position = in_Position;\n"\
+        "  ex_Color = in_Color;\n"\
+        "}\n"
+};
 
-        "layout(location=0) in vec4 in_Position;\n"
-        "layout(location=1) in vec4 in_Color;\n"
-        "out vec4 ex_Color;\n"
-
-        "void main(void)\n"
-        "{\n"
-        "  gl_Position = in_Position;\n"
-        "  ex_Color = in_Color;\n"
-        "}\n"};
-// Shader-ul de fragment / Fragment shader (este privit ca un sir de caractere)
 const GLchar *FragmentShader = {
-        "#version 400\n"
+        "#version 400\n"\
 
-        "in vec4 ex_Color;\n"
-        "out vec4 out_Color;\n"
+        "in vec4 ex_Color;\n"\
+        "out vec4 out_Color;\n"\
 
-        "void main(void)\n"
-        "{\n"
-        "  out_Color = ex_Color;\n"
-        "}\n"};
+        "void main(void)\n"\
+        "{\n"\
+        "  out_Color = ex_Color;\n"\
+        "}\n"
+};
 
-void CreateVBO(void) {
-    // varfurile
-    GLfloat Vertices[] = {
-            -0.8f, -0.8f, 0.0f, 1.0f,
-            0.0f, 0.8f, 0.0f, 1.0f,
-            0.8f, -0.8f, 0.0f, 1.0f
+void initialize() {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+}
+
+void drawRGBCube() {
+    GLfloat vertices[] = {
+            // Green - Yellow - Cyan
+            -0.3f, -0.3f, 1.0f, 1.0f,
+            0.3f, -0.3f, 1.0f, 1.0f,
+            -0.3f, 0.3f, 1.0f, 1.0f,
+            // Yellow - Cyan - White
+            0.3f, -0.3f, 1.0f, 1.0f,
+            -0.3f, 0.3f, 1.0f, 1.0f,
+            0.3f, 0.3f, 1.0f, 1.0f,
+            // Yellow - White - Red
+            0.3f, -0.3f, 1.0f, 1.0f,
+            0.3f, 0.3f, 1.0f, 1.0f,
+            0.5f, 0.1f, 1.0f, 1.0f,
+            // White - Red - Magenta
+            0.3f, 0.3f, 1.0f, 1.0f,
+            0.5f, 0.1f, 1.0f, 1.0f,
+            0.5f, 0.7f, 1.0f, 1.0f,
+            // Cyan - White - Magenta
+            -0.3f, 0.3f, 1.0f, 1.0f,
+            0.3f, 0.3f, 1.0f, 1.0f,
+            0.5f, 0.7f, 1.0f, 1.0f,
+            // Cyan - Magenta - Blue
+            -0.3f, 0.3f, 1.0f, 1.0f,
+            0.5f, 0.7f, 1.0f, 1.0f,
+            0.0f, 0.7f, 1.0f, 1.0f,
     };
-
-    // culorile, ca atribute ale varfurilor
-    GLfloat Colors[] = {
-            1.0f, 0.0f, 0.0f, 1.0f,
+    GLfloat color[] = {
+            // Green - Yellow - Cyan
             0.0f, 1.0f, 0.0f, 1.0f,
-            //  0.0f, 0.0f, 1.0f, 1.0f
+            1.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f,
+            // Yellow - Cyan - White
+            1.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f, 1.0f,
+            // Yellow - White - Red
+            1.0f, 1.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            // White - Red - Magenta
+            1.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f,
+            // Cyan - White - Magenta
+            0.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f,
+            // Cyan - Magenta - Blue
+            0.0f, 1.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
     };
 
-    // se creeaza un buffer nou
-    glGenBuffers(1, &VboId);
-    // este setat ca buffer curent
-    glBindBuffer(GL_ARRAY_BUFFER, VboId);
-    // punctele sunt "copiate" in bufferul curent
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-    // se creeaza / se leaga un VAO (Vertex Array Object) - util cand se
-    // utilizeaza mai multe VBO
-    glGenVertexArrays(1, &VaoId);
-    glBindVertexArray(VaoId);
-    // se activeaza lucrul cu atribute; atributul 0 = pozitie
+    glGenBuffers(1, &VboId1);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    //
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-    // un nou buffer, pentru culoare
-    glGenBuffers(1, &ColorBufferId);
-    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
-    // atributul 1 =  culoare
+    glGenBuffers(1, &ColorBufferId1);
+    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
 }
 
-void DestroyVBO(void) {
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &ColorBufferId);
-    glDeleteBuffers(1, &VboId);
-
-    glBindVertexArray(0);
-    glDeleteVertexArrays(1, &VaoId);
+void createVBO() {
+    drawRGBCube();
 }
 
-void CreateShaders(void) {
+void createShaders() {
     VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+    glShaderSource(VertexShaderId, 1, &VertexShader, nullptr);
     glCompileShader(VertexShaderId);
 
     FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+    glShaderSource(FragmentShaderId, 1, &FragmentShader, nullptr);
     glCompileShader(FragmentShaderId);
 
     ProgramId = glCreateProgram();
@@ -103,7 +126,11 @@ void CreateShaders(void) {
     glUseProgram(ProgramId);
 }
 
-void DestroyShaders(void) {
+void destroyVBO() {
+
+}
+
+void destroyShaders() {
     glUseProgram(0);
 
     glDetachShader(ProgramId, VertexShaderId);
@@ -115,34 +142,33 @@ void DestroyShaders(void) {
     glDeleteProgram(ProgramId);
 }
 
-void Initialize(void) {
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);  // culoarea de fond a ecranului
+void cleanUp() {
+    destroyShaders();
+    destroyVBO();
 }
 
-void RenderFunction(void) {
+void renderFunction() {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    CreateVBO();
-    CreateShaders();
-    glPointSize(20.0);
-    glDrawArrays(GL_POINTS, 0, 3);
+    createVBO();
+    createShaders();
+    glDrawArrays(GL_TRIANGLES, 0, 18);
     glFlush();
 }
 
-void Cleanup(void) {
-    DestroyShaders();
-    DestroyVBO();
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(100, 100);  // pozitia initiala a ferestrei
-    glutInitWindowSize(700, 500);      // dimensiunile ferestrei
-    glutCreateWindow("Primul triunghi - OpenGL <<nou>>");  // titlul ferestrei
-    glewInit();  // nu uitati de initializare glew; trebuie initializat inainte de
-    // a a initializa desenarea
-    Initialize();
-    glutDisplayFunc(RenderFunction);
-    glutCloseFunc(Cleanup);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Cub RGB");
+    glewInit();
+
+    initialize();
+
+    glutDisplayFunc(renderFunction);
+    glutCloseFunc(cleanUp);
     glutMainLoop();
+
+    return 0;
 }
