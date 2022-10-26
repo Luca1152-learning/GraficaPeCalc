872d47402aeff1440e71092c35cd567d0e802712
Luca Trușcă <t.luca1152@gmail.com>
2022-10-26 08:10:15 +0300

    Solution with triangles

--- src/main.cpp
+++ src/main.cpp
@@ -4,98 +4,135 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-// Old OpenGL
-//void init(void)  // initializare fereastra de vizualizare
-//{
-//    glClearColor(1.0, 1.0, 1.0, 0.0); // precizeaza culoarea de fond a ferestrei de vizualizare
-//
-//    glMatrixMode(GL_PROJECTION);  // se precizeaza este vorba de o reprezentare 2D, realizata prin proiectie ortogonala
-//    gluOrtho2D(0, 800.0, 0.0, 700.0); // sunt indicate coordonatele extreme ale ferestrei de vizualizare
-//}
-//
-//void desen(void) // procedura desenare
-//{
-//    // dreptunghi: desenare directa
-//    glColor3f(0.0, 0.0, 1.0);
-//    glRecti(20, 130, 140, 310);
-//
-//
-//    //poligon convex
-//    glColor3f(1.0, 0.0, 0.0);
-//    glBegin(GL_POLYGON);
-//    glVertex2i(0, 0);
-//    glVertex2i(100, 10);
-//    glVertex2i(200, 120);
-//    glVertex2i(155, 290);
-//    glEnd();
-//
-//
-//
-//
-//    // evantai de triunghiuri
-//    glColor3d(1.0, 0.0, 0.25);
-//    glBegin(GL_TRIANGLE_FAN);
-//    glVertex2i(250, 300);
-//    glVertex2i(50, 300);
-//    glVertex2i(200, 350);
-//    glVertex2i(250, 500);
-//    glVertex2i(350, 200);
-//    glEnd();
-//
-//    // reuniune de triunghiuri
-//    glColor3d(1.0, 0.75, 0.25);
-//    glBegin(GL_TRIANGLE_STRIP);
-//    glVertex2i(650, 300);
-//    glVertex2i(450, 300);
-//    glVertex2i(600, 350);
-//    glVertex2i(650, 500);
-//    glVertex2i(750, 200);
-//    glEnd();
-//
-//    glFlush(); // proceseaza procedurile OpenGL cat mai rapid
-//}
-//
-//void desen2(void)
-//{
-//
-//    glColor3f(1, 0, 0);
-//    glRecti(-30, 20, 40, 10);
-//    //
-//    glColor3f(1, 1, 0);
-//    glMatrixMode(GL_MODELVIEW);
-//    glPushMatrix();
-//    glTranslatef(-10.0, -20.0, 0.0);
-//    glScalef(0.5, 2.0, 0.0);
-//    glRecti(-30, 20, 40, 10);
-//    glPopMatrix();
-//
-//    glPointSize(4.0);
-//    glColor3f(0, 0, 0);
-//    glBegin(GL_POINTS);
-//    glVertex3f(40, 20, 0);
-//    glEnd();
-//
-//    glPointSize(8.0);
-//    glColor3f(0, 0, 0);
-//    glBegin(GL_POINTS);
-//    glVertex3f(10, 20, 0);
-//    glEnd();
-//
-//    glFlush();
-//
-//}
-
 // New OpenGL
-void initialize(void) {
+GLuint VboId1,
+        ColorBufferId1,
+        VertexShaderId,
+        FragmentShaderId,
+        ProgramId;
+
+const GLchar *VertexShader = {
+        "#version 400\n"\
+        "layout(location=0) in vec4 in_Position;\n"\
+        "layout(location=1) in vec4 in_Color;\n"\
+        "out vec4 ex_Color;\n"\
+        "void main(void)\n"\
+        "{\n"\
+        "  gl_Position = in_Position;\n"\
+        "  ex_Color = in_Color;\n"\
+        "}\n"
+};
+
+const GLchar *FragmentShader = {
+        "#version 400\n"\
+
+        "in vec4 ex_Color;\n"\
+        "out vec4 out_Color;\n"\
+
+        "void main(void)\n"\
+        "{\n"\
+        "  out_Color = ex_Color;\n"\
+        "}\n"
+};
+
+void initialize() {
     glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 }
 
-void createVBO() {
+void drawBlueRectangle() {
+    GLfloat vertices[] = {
+            // Blue rectangle
+            -0.95f, -0.675f, 0.0f, 1.0f,
+            -0.65f, -0.675f, 0.0f, 1.0f,
+            -0.95f, 0.03f, 0.0f, 1.0f,
+            -0.65f, -0.675f, 0.0f, 1.0f,
+            -0.65f, 0.03f, 0.0f, 1.0f,
+            -0.95f, 0.03f, 0.0f, 1.0f,
+            // Red polygon 1
+            -1.0f, -1.0f, 0.0f, 1.0f,
+            -0.75f, -0.966f, 0.0f, 1.0f,
+            -0.612f, -0.03f, 0.0f, 1.0f,
+            -0.75f, -0.966f, 0.0f, 1.0f,
+            -0.612f, -0.03f, 0.0f, 1.0f,
+            -0.5f, -0.6f, 0.0f, 1.0f,
+            // Red polygon 2
+            -0.375f, 0.0f, 0.0f, 1.0f,
+            -0.875f, 0.0f, 0.0f, 1.0f,
+            -0.5f, 0.16f, 0.0f, 1.0f,
+            -0.5f, 0.16f, 0.0f, 1.0f,
+            -0.375f, 0.666f, 0.0f, 1.0f,
+            -0.125f, -0.333f, 0.0f, 1.0f,
+            // Yellow polygon
+            0.625f, 0.0f, 0.0f, 1.0f,
+            0.125f, 0.0f, 0.0f, 1.0f,
+            0.625f, 0.666f, 0.0f, 1.0f,
+            0.5f, 0.166f, 0.0f, 1.0f,
+            0.625f, 0.666f, 0.0f, 1.0f,
+            0.875f, -0.333f, 0.0f, 1.0f,
 
+    };
+    GLfloat color[] = {
+            // Blue rectangle
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f,
+            // Red polygon 1
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            // Red polygon 2
+            1.0f, 0.0f, 0.25f, 1.0f,
+            1.0f, 0.0f, 0.25f, 1.0f,
+            1.0f, 0.0f, 0.25f, 1.0f,
+            1.0f, 0.0f, 0.25f, 1.0f,
+            1.0f, 0.0f, 0.25f, 1.0f,
+            1.0f, 0.0f, 0.25f, 1.0f,
+            // Yellow polygon
+            1.0f, 0.75f, 0.25f, 1.0f,
+            1.0f, 0.75f, 0.25f, 1.0f,
+            1.0f, 0.75f, 0.25f, 1.0f,
+            1.0f, 0.75f, 0.25f, 1.0f,
+            1.0f, 0.75f, 0.25f, 1.0f,
+            1.0f, 0.75f, 0.25f, 1.0f,
+    };
+
+    glGenBuffers(1, &VboId1);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glGenBuffers(1, &ColorBufferId1);
+    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+}
+
+void createVBO() {
+    drawBlueRectangle();
 }
 
 void createShaders() {
+    VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VertexShaderId, 1, &VertexShader, nullptr);
+    glCompileShader(VertexShaderId);
 
+    FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FragmentShaderId, 1, &FragmentShader, nullptr);
+    glCompileShader(FragmentShaderId);
+
+    ProgramId = glCreateProgram();
+    glAttachShader(ProgramId, VertexShaderId);
+    glAttachShader(ProgramId, FragmentShaderId);
+    glLinkProgram(ProgramId);
+    glUseProgram(ProgramId);
 }
 
 void destroyVBO() {
@@ -103,20 +140,28 @@ void destroyVBO() {
 }
 
 void destroyShaders() {
+    glUseProgram(0);
 
+    glDetachShader(ProgramId, VertexShaderId);
+    glDetachShader(ProgramId, FragmentShaderId);
+
+    glDeleteShader(FragmentShaderId);
+    glDeleteShader(VertexShaderId);
+
+    glDeleteProgram(ProgramId);
 }
 
-void cleanUp(void) {
+void cleanUp() {
     destroyShaders();
     destroyVBO();
 }
 
-void renderFunction(void) {
+void renderFunction() {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     createVBO();
     createShaders();
-    glPointSize(20.0);
-    glDrawArrays(GL_POINTS, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 24);
     glFlush();
 }
 
@@ -124,8 +169,8 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(100, 100);
-    glutInitWindowSize(700, 500);
-    glutCreateWindow("Primul triunghi - OpenGL <<nou>>");
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Poligoane");
     glewInit();
 
     initialize();
