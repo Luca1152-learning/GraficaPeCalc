678b1899d9922653d0a95fe52ff052e09d8a18fc
Luca Trușcă <t.luca1152@gmail.com>
2022-10-26 18:32:49 +0300

    Add initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,32 @@
 // Codul sursa este adaptat dupa OpenGLBook.com
+
+#include <windows.h>  // biblioteci care urmeaza sa fie incluse
 #include <stdlib.h> // necesare pentru citirea shader-elor
 #include <stdio.h>
 #include <GL/glew.h> // glew apare inainte de freeglut
 #include <GL/freeglut.h> // nu trebuie uitat freeglut.h
+#include "loadShaders.h"
 
 //////////////////////////////////////
 
-GLuint VaoId, VboId, ColorBufferId, VertexShaderId, FragmentShaderId, ProgramId;
+GLuint
+        VaoId,
+        VboId,
+        ColorBufferId,
+        ProgramId,
+        codColLocation;
 
-///////////////////////////////////////////
+int codCol;
 
-// Shader-ul de varfuri / Vertex shader (este privit ca un sir de caractere)
-const GLchar *VertexShader = {
-        "#version 400\n"
-
-        "layout(location=0) in vec4 in_Position;\n"
-        "layout(location=1) in vec4 in_Color;\n"
-        "out vec4 ex_Color;\n"
-
-        "void main(void)\n"
-        "{\n"
-        "  gl_Position = in_Position;\n"
-        "  ex_Color = in_Color;\n"
-        "}\n"};
-// Shader-ul de fragment / Fragment shader (este privit ca un sir de caractere)
-const GLchar *FragmentShader = {
-        "#version 400\n"
-
-        "in vec4 ex_Color;\n"
-        "out vec4 out_Color;\n"
-
-        "void main(void)\n"
-        "{\n"
-        "  out_Color = ex_Color;\n"
-        "}\n"};
-
-void CreateVBO(void) {
+void CreateVBO(void)
+{
     // varfurile
     GLfloat Vertices[] = {
+            -0.2f, -0.2f, 0.0f, 1.0f,
+            0.0f,  0.2f, 0.0f, 1.0f,
+            0.2f, -0.2f, 0.0f, 1.0f,
             -0.8f, -0.8f, 0.0f, 1.0f,
-            0.0f, 0.8f, 0.0f, 1.0f,
+            0.0f,  0.8f, 0.0f, 1.0f,
             0.8f, -0.8f, 0.0f, 1.0f
     };
 
@@ -47,23 +34,24 @@ void CreateVBO(void) {
     GLfloat Colors[] = {
             1.0f, 0.0f, 0.0f, 1.0f,
             0.0f, 1.0f, 0.0f, 1.0f,
-            //  0.0f, 0.0f, 1.0f, 1.0f
+            0.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 0.0f, 0.0f, 1.0f,
+            0.0f, 1.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f, 1.0f
     };
 
     // se creeaza un buffer nou
     glGenBuffers(1, &VboId);
     // este setat ca buffer curent
     glBindBuffer(GL_ARRAY_BUFFER, VboId);
-    // punctele sunt "copiate" in bufferul curent
+    // varfurile sunt "copiate" in bufferul curent
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
-    // se creeaza / se leaga un VAO (Vertex Array Object) - util cand se
-    // utilizeaza mai multe VBO
+    // se creeaza / se leaga un VAO (Vertex Array Object) - util cand se utilizeaza mai multe VBO
     glGenVertexArrays(1, &VaoId);
     glBindVertexArray(VaoId);
     // se activeaza lucrul cu atribute; atributul 0 = pozitie
     glEnableVertexAttribArray(0);
-    //
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
     // un nou buffer, pentru culoare
@@ -74,8 +62,8 @@ void CreateVBO(void) {
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
 }
-
-void DestroyVBO(void) {
+void DestroyVBO(void)
+{
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
 
@@ -87,60 +75,58 @@ void DestroyVBO(void) {
     glDeleteVertexArrays(1, &VaoId);
 }
 
-void CreateShaders(void) {
-    VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
-    glCompileShader(VertexShaderId);
-
-    FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
-    glCompileShader(FragmentShaderId);
-
-    ProgramId = glCreateProgram();
-    glAttachShader(ProgramId, VertexShaderId);
-    glAttachShader(ProgramId, FragmentShaderId);
-    glLinkProgram(ProgramId);
+void CreateShaders(void)
+{
+    ProgramId = LoadShaders("shaders/02_01_Shader.vert", "shaders/02_01_Shader.frag");
     glUseProgram(ProgramId);
 }
-
-void DestroyShaders(void) {
-    glUseProgram(0);
-
-    glDetachShader(ProgramId, VertexShaderId);
-    glDetachShader(ProgramId, FragmentShaderId);
-
-    glDeleteShader(FragmentShaderId);
-    glDeleteShader(VertexShaderId);
-
+void DestroyShaders(void)
+{
     glDeleteProgram(ProgramId);
 }
 
-void Initialize(void) {
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);  // culoarea de fond a ecranului
-}
-
-void RenderFunction(void) {
-    glClear(GL_COLOR_BUFFER_BIT);
+void Initialize(void)
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f); // culoarea de fond a ecranului
     CreateVBO();
     CreateShaders();
+}
+void RenderFunction(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Variabilele uniforme sunt folosite pentru a "comunica" cu shader-ele
+    codColLocation = glGetUniformLocation(ProgramId, "codCol");
+    codCol = 0;
+    glUniform1i(codColLocation, codCol);
+    // Desenarea segmentelor folosind modelul Gouraud (Gouraud shading)
+    glLineWidth(5.0);
+    glDrawArrays(GL_LINE_STRIP, 0, 6);
+
+    // Se atribuie o alta valoare variabilei uniforme
+    codCol = 1;
+    glUniform1i(codColLocation, codCol);
+    // Punctele cu o singura culoare
+    glEnable(GL_POINT_SMOOTH);
     glPointSize(20.0);
-    glDrawArrays(GL_POINTS, 0, 3);
+    glDrawArrays(GL_POINTS, 0, 6);
+    glDisable(GL_POINT_SMOOTH);
     glFlush();
 }
-
-void Cleanup(void) {
+void Cleanup(void)
+{
     DestroyShaders();
     DestroyVBO();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(100, 100);  // pozitia initiala a ferestrei
-    glutInitWindowSize(700, 500);      // dimensiunile ferestrei
-    glutCreateWindow("Primul triunghi - OpenGL <<nou>>");  // titlul ferestrei
-    glewInit();  // nu uitati de initializare glew; trebuie initializat inainte de
-    // a a initializa desenarea
+    glutInitWindowPosition(100, 100); // pozitia initiala a ferestrei
+    glutInitWindowSize(600, 400); //dimensiunile ferestrei
+    glutCreateWindow("Primitive si culori"); // titlul ferestrei
+    glewInit(); // nu uitati de initializare glew; trebuie initializat inainte de a a initializa desenarea
     Initialize();
     glutDisplayFunc(RenderFunction);
     glutCloseFunc(Cleanup);
