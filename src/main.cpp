4795bab718515e5ff5642bc56018a2d96a996b04
Luca Trușcă <t.luca1152@gmail.com>
2022-10-30 22:48:54 +0200

    Reorder functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
 #include <GL/glew.h>
-#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include <iostream>
 
-
-GLFWwindow* Window;
+GLFWwindow *Window;
 GLuint ProgramId, VaoId, VboId, ColorBufferId;
 
-const char* VertexShaderSource = R"SHADER-SOURCE(
+const char *VertexShaderSource = R"SHADER-SOURCE(
 #version 330 core
 
 layout (location = 0) in vec4 in_Position;
@@ -27,7 +20,7 @@ void main() {
 }
 )SHADER-SOURCE";
 
-const char* FragmentShaderSource = R"SHADER-SOURCE(
+const char *FragmentShaderSource = R"SHADER-SOURCE(
 #version 330 core
 
 in vec4 ex_Color;
@@ -38,114 +31,12 @@ void main() {
 }
 )SHADER-SOURCE";
 
-void Initialize();
-void Render();
-void Cleanup();
-
-GLuint LoadShaders(const char* vertexSource, const char* fragSource);
-
-int main() {
-    if (!glfwInit()) {
-        exit(EXIT_FAILURE);
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make macOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    Window = glfwCreateWindow(640, 480, "macOS OpenGL", nullptr, nullptr);
-    if (!Window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    glfwMakeContextCurrent(Window);
-    glewInit();
-    glfwSwapInterval(1);
-
-    Initialize();
-
-    while (!glfwWindowShouldClose(Window)) {
-        int width, height;
-        glfwGetFramebufferSize(Window, &width, &height);
-
-        glViewport(0, 0, width, height);
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        Render();
-
-        glfwSwapBuffers(Window);
-        glfwPollEvents();
-    }
-
-    Cleanup();
-
-    glfwDestroyWindow(Window);
-    glfwTerminate();
-    return 0;
-}
-
-void Initialize() {
-    GLfloat vertices[] = {
-            -0.5, -0.5, 0.0, 1.0,
-            0.0,  0.5, 0.0, 1.0,
-            0.5, -0.5, 0.0, 1.0,
-    };
-
-    GLfloat colors[] = {
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-    };
-
-    glGenVertexArrays(1, &VaoId);
-    glBindVertexArray(VaoId);
-
-    glGenBuffers(1, &VboId);
-    glBindBuffer(GL_ARRAY_BUFFER, VboId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glGenBuffers(1, &ColorBufferId);
-    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    ProgramId = LoadShaders(VertexShaderSource, FragmentShaderSource);
-}
-
-void Render() {
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    glFlush();
-}
-
-void Cleanup() {
-    glDeleteProgram(ProgramId);
-
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &ColorBufferId);
-    glDeleteBuffers(1, &VboId);
-
-    glBindVertexArray(0);
-    glDeleteVertexArrays(1, &VaoId);
-}
-
-GLuint LoadShaders(const char* vertexSource, const char* fragSource) {
+GLuint loadShaders(const char *vertexSource, const char *fragSource) {
     GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShaderId, 1, &vertexSource, nullptr);
     glCompileShader(vertexShaderId);
 
-    int  success;
+    int success;
     char infoLog[512];
     glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
 
@@ -172,4 +63,101 @@ GLuint LoadShaders(const char* vertexSource, const char* fragSource) {
     glUseProgram(programId);
 
     return programId;
+}
+
+void initialize() {
+    GLfloat vertices[] = {
+            -0.5, -0.5, 0.0, 1.0,
+            0.0, 0.5, 0.0, 1.0,
+            0.5, -0.5, 0.0, 1.0,
+    };
+
+    GLfloat colors[] = {
+            1.0, 0.0, 0.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,
+            0.0, 0.0, 1.0, 1.0,
+    };
+
+    glGenVertexArrays(1, &VaoId);
+    glBindVertexArray(VaoId);
+
+    glGenBuffers(1, &VboId);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glGenBuffers(1, &ColorBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    ProgramId = loadShaders(VertexShaderSource, FragmentShaderSource);
+}
+
+void render() {
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glFlush();
+}
+
+void cleanUp() {
+    glDeleteProgram(ProgramId);
+
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, &ColorBufferId);
+    glDeleteBuffers(1, &VboId);
+
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &VaoId);
+}
+
+int main() {
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    Window = glfwCreateWindow(640, 480, "macOS OpenGL", nullptr, nullptr);
+    if (!Window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(Window);
+    glewInit();
+    glfwSwapInterval(1);
+
+    initialize();
+
+    while (!glfwWindowShouldClose(Window)) {
+        int width, height;
+        glfwGetFramebufferSize(Window, &width, &height);
+
+        glViewport(0, 0, width, height);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        render();
+
+        glfwSwapBuffers(Window);
+        glfwPollEvents();
+    }
+
+    cleanUp();
+
+    glfwDestroyWindow(Window);
+    glfwTerminate();
+
+    return 0;
 }
