8aa01b67fd8f85ed975d417958dbae09adc58dbd
Luca Trușcă <t.luca1152@gmail.com>
2022-11-01 23:13:20 +0200

    Almost make colors work

--- src/House.h
+++ src/House.h
@@ -14,7 +14,7 @@
 
 class House {
 private:
-    GLuint vaoId, vboId, eboId;
+    GLuint vaoId, vboId, colorId, verticesEboId, colorsEboId, vaoId2;
 
     Color wallColor = Color(255, 0, 255);
     Color roofColor = Color(255, 0, 0);
@@ -27,23 +27,43 @@ private:
                 0.5f, 0.5f, // 2 top right
                 -0.5f, 0.5f // 3 top left
         };
-        GLuint indices[] = {
+        GLuint verticesIndices[] = {
                 0, 1, 2,
                 0, 2, 3,
         };
+        GLfloat colors[] = {
+                1.0f, 0.0f, 0.0f, 1.0f, // 0
+                0.0f, 1.0f, 0.0f, 1.0f, // 0
+                0.0f, 0.0f, 1.0f, 1.0f, // 0
+        };
+        GLuint colorsIndices[] = {
+                0, 0, 0,
+                0, 0, 0,
+        };
 
-        glGenVertexArrays(1, &vaoId);
+
+//        glGenVertexArrays(1, &vaoId);
+//        glBindVertexArray(vaoId);
+
         glGenBuffers(1, &vboId);
-        glGenBuffers(1, &eboId);
-
-        glBindVertexArray(vaoId);
+        glGenBuffers(1, &verticesEboId);
         glBindBuffer(GL_ARRAY_BUFFER, vboId);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, verticesEboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(verticesIndices), verticesIndices, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
-        glBindVertexArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+
+        glGenBuffers(1, &colorId);
+        glGenBuffers(1, &colorsEboId);
+        glBindBuffer(GL_ARRAY_BUFFER, colorId);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, colorsEboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(colorsIndices), colorsIndices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr); // TODO
+
+//        glBindVertexArray(0);
     }
 
 public:
@@ -52,9 +72,17 @@ public:
     }
 
     void render() {
-        glBindVertexArray(vaoId);
+//        glBindVertexArray(vaoId);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, verticesEboId);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, colorId);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, colorsEboId);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+//        glBindVertexArray(0);
     }
 };
 
