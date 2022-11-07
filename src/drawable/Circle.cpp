6f92865a0b3dcc0276f5027328d7ad4c18eb9ed8
Luca Trușcă <t.luca1152@gmail.com>
2022-11-07 11:29:15 +0200

    Refactor Circle

--- src/drawable/Circle.cpp
+++ src/drawable/Circle.cpp
@@ -1,1 +1,31 @@
 #include "Circle.h"
+
+void Circle::setupTriangles() {
+    vector<Vector> triangleVerticesVec;
+    vector<Vector> triangleVerticesColorsVec;
+
+    for (int i = 1; i <= 360; i++) {
+        float currentX = cos(radians((float) i)), currentY = sin(radians((float) i));
+        float previousX = cos(radians((float) i - 1)), previousY = sin(radians((float) i - 1));
+
+        // Add vertices
+        triangleVerticesVec.push_back({0, 0});
+        triangleVerticesVec.push_back({currentX, currentY});
+        triangleVerticesVec.push_back({previousX, previousY});
+
+        // Add colors
+        triangleVerticesColorsVec.push_back(color);
+        triangleVerticesColorsVec.push_back(color);
+        triangleVerticesColorsVec.push_back(color);
+    }
+
+
+    triangleVertices = {4, triangleVerticesVec};
+    triangleVerticesColors = {4, triangleVerticesColorsVec};
+}
+
+Circle::Circle(float centerX, float centerY, float radius, Color color) :
+        Drawable(centerX, centerY, radius, radius), color(color) {
+    setupTriangles();
+    setupVao();
+}
--- src/drawable/Circle.h
+++ src/drawable/Circle.h
@@ -9,39 +9,12 @@ using namespace glm;
 
 class Circle : public Drawable {
 private:
-    float radius;
     Color color;
 
-    void setupTriangles() final override {
-        vector<Vector> triangleVerticesVec;
-        vector<Vector> triangleVerticesColorsVec;
-
-        for (int i = 1; i <= 360; i++) {
-            float currentX = cos(radians((float) i)), currentY = sin(radians((float) i));
-            float previousX = cos(radians((float) i - 1)), previousY = sin(radians((float) i - 1));
-
-            // Add vertices
-            triangleVerticesVec.push_back({0, 0});
-            triangleVerticesVec.push_back({currentX, currentY});
-            triangleVerticesVec.push_back({previousX, previousY});
-
-            // Add colors
-            triangleVerticesColorsVec.push_back(color);
-            triangleVerticesColorsVec.push_back(color);
-            triangleVerticesColorsVec.push_back(color);
-        }
-
-
-        triangleVertices = {4, triangleVerticesVec};
-        triangleVerticesColors = {4, triangleVerticesColorsVec};
-    }
+    void setupTriangles() final override;
 
 public:
-    Circle(float centerX, float centerY, float radius, Color color) :
-            Drawable(centerX, centerY, radius, radius), color(color) {
-        setupTriangles();
-        setupVao();
-    }
+    Circle(float centerX, float centerY, float radius, Color color);
 };
 
 
